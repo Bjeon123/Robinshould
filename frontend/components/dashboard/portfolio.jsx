40b0a7193507404f6de02a4eb5fffe0270c1bb51
Samuel Jeon <samueljeon99@gmail.com>
2021-09-09 14:54:40 -0400

    chart practice

--- frontend/components/dashboard/portfolio.jsx
+++ frontend/components/dashboard/portfolio.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import LineGraph from '../charts/line_graph'
 
 class Portfolio extends React.Component{
     constructor(props){
         super(props)
+
     }
+
+    
+
     render(){
         return(
             <div className="portfolio">
                 <h1>{`$${this.props.user.total_capital.toFixed(2)}`}</h1>
+                <LineGraph/>
             </div>
         )
     }
