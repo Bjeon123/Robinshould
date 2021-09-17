4c304592091af847d049fa564a8113639cf5e132
Samuel Jeon <samueljeon99@gmail.com>
2021-09-17 09:47:31 -0400

    bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,13 @@ require 'csv'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create!(email: "demo@demo.com", first_name: "demo", last_name: "user", password: '123456', address: "demo st", total_capital:10000.50)
+User.delete_all
+Stock.delete_all
+Holding.delete_all
+Watchlist.delete_all
+WatchlistJoin.delete_all
+
+User.create!(email: "demo@demo.com", first_name: "demo", last_name: "user", password: '123456', address: "demo st", total_capital:10000.50)
 
 CSV.foreach(("app/assets/stock_info/stockTickerAndName.csv"), headers: true, col_sep: ",") do |row|
     Stock.create!(ticker: row[0],company_name: row[1])
