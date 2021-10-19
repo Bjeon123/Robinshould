0101f1818964d609d96b0341f6345744bc7b98c2
Sam Jeon <samueljeon99@gmail.com>
2021-10-19 01:39:49 -0400

    fix splash styling

--- frontend/components/sessions/signin_form.jsx
+++ frontend/components/sessions/signin_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 class LoginForm extends React.Component{
     constructor(props){
         super(props);
@@ -6,7 +7,7 @@ class LoginForm extends React.Component{
             first_name: "",
             last_name: "",
             email: "",
-            address: "",
+            address: "none",
             password: "",
             total_capital: 0
         }
@@ -30,43 +31,83 @@ class LoginForm extends React.Component{
     }
 
     renderErrors() {
-        return (
-            <ul>
-                {this.props.errors.map((error, i) => (
+        let errors=[]
+        for(let i=0;i< this.props.errors.length;i++){
+            const error = this.props.errors[i];
+            if(error === "First name can't be blank"){
+                errors.push(
+                    <li className="error" key={`error-${i}`}>
+                        Please enter your first name.
+                    </li>
+                )
+            }
+            else if(error ==="Last name can't be blank"){
+                errors.push(
+                    <li className="error" key={`error-${i}`}>
+                        Please enter your last name.
+                    </li>
+                )
+            }
+            else if(error ==="Password is too short (minimum is 6 characters)")
+            {
+                errors.push(
+                    <li className="error" key={`error-${i}`}>
+                        Your password must be at least 6 characters.
+                    </li>
+                )
+            }
+            else if(error === "Email can't be blank"){
+                errors.push(
+                    <li className="error" key={`error-${i}`}>
+                        Please enter your email.
+                    </li>
+                )
+            }
+            else{
+                errors.push(
                     <li className="error" key={`error-${i}`}>
                         {error}
                     </li>
-                ))}
+                )
+            }
+        }
+        return (
+            <ul>
+                {errors}
             </ul>
         );
     }
 
     render(){
+        console.log(this.props)
         return(
             <div className="signup-page-container">
                 <div className="signup-page">
-                    <img width="30%" src={window.logo}/>
+                    <Link to={'/'}>
+                        <img width="30%" src={window.logo}/>
+                    </Link>
+                    <div className="signup-header">
+                        <h1>Make Your Money Move</h1>
+                        <p>Robinshould lets you invest in companies you love, commission-free.</p>
+                    </div>
+                    <p id="caution">Please enter your full legal name. Your legal name should match any form of government ID.</p>
                     <form className="signup-form">
                         <div className="name-form">
-                            <label> 
                                 <input placeholder="First Name" onChange={this.handleChange('first_name')} type="text" value={this.state.first_name} />
-                            </label>
                             <br />
-                            <label> 
                                 <input placeholder="Last Name" onChange={this.handleChange('last_name')} type="text" value={this.state.last_name} />
-                            </label>
                         </div>
                         <br />
                         <label>
                             <input className="signup-input" placeholder="Email" onChange={this.handleChange('email')} type="text" value={this.state.email} />
                         </label>
-                        <br />
+                        {/* <br />
                         <label>
                             <input className="signup-input" placeholder="Address" onChange={this.handleChange('address')} type="text" value={this.state.address} />
-                        </label>
+                        </label> */}
                         <br />
                         <label>
-                            <input className="signup-input" placeholder="Password" onChange={this.handleChange('password')} type="password" value={this.state.password} />
+                            <input className="signup-input" placeholder="Password (min. 6 characters)" onChange={this.handleChange('password')} type="password" value={this.state.password} />
                         </label>
                         <br />
                         <button onClick={this.handleSubmit}>Continue</button>
@@ -77,9 +118,9 @@ class LoginForm extends React.Component{
                 </div>
                 <div className="signup-page-description">
                     <h1>Commission-free trading</h1>
-                    <p>Break free from commission-fees and make unlimited commission-free trades in stocks, funds, and options with Robinhood Financial. Other fees may apply. View our fee schedule to learn more.</p>
+                    <p>Break free from commission-fees and make unlimited commission-free trades in stocks, funds, and options with Robinshould Financial. Other fees may apply. View our fee schedule to learn more.</p>
                     <h1>Account Protection</h1>
-                    <p>Robinhood Financial is a member of SIPC. Securities in your account protected up to $500,000. For details, please see www.sipc.org.</p>
+                    <p>Robinshould Financial is a member of SIPC. Securities in your account protected up to $500,000. For details, please see www.sipc.org.</p>
                     <h1>Stay on top of your portfolio</h1>
                     <p>Set up customized news and notifications to stay on top of your assets as casually or as relentlessly as you like. Controlling the flow of info is up to you.</p>
                 </div>
--- frontend/util/stock_api_util.js
+++ frontend/util/stock_api_util.js
@@ -6,7 +6,7 @@ const token = 'pk_b33e3727ae694c11b9275f5ba6e0d253';
 //     $.ajax({
 //         method: "GET",
 //https://cloud.iexapis.com/
-//         url: `https://cloud.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=5dm&token=pk_b33e3727ae694c11b9275f5ba6e0d253`
+//         url: `https://sandbox.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=5dm&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`
 //     })
 // )
 
@@ -42,93 +42,93 @@ export const fetchStockTickers = (userId) => (
 export const fetchintradayData =(ticker) =>(
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/${ticker}/batch?&types=intraday-prices,news&last=4&token=pk_b33e3727ae694c11b9275f5ba6e0d253`
+        url: `https://sandbox.iexapis.com/v1/stock/${ticker}/batch?&types=intraday-prices,news&last=4&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`
     })
 )
 
 export const fetchAllQuotes = (tickerArr) =>(
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/market/batch?&types=price,intraday-prices,news&symbols=${tickerArr.join(',')}&token=pk_b33e3727ae694c11b9275f5ba6e0d253`,
+        url: `https://sandbox.iexapis.com/v1/stock/market/batch?&types=price,intraday-prices,news&symbols=${tickerArr.join(',')}&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`,
     })
 )
 
 export const getNews = (tickerArr) =>(
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/market/batch?&types=news&symbols=${tickerArr.join(',')}&last=1&token=pk_b33e3727ae694c11b9275f5ba6e0d253`,
+        url: `https://sandbox.iexapis.com/v1/stock/market/batch?&types=news&symbols=${tickerArr.join(',')}&last=1&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`,
     })
 )
 
 export const fetchStockWeekData= (ticker) =>(
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=5dm&token=pk_b33e3727ae694c11b9275f5ba6e0d253`
+        url: `https://sandbox.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=5dm&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`
     })
 )
 export const fetchStockMonthData= (ticker) =>(
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=1mm&token=pk_b33e3727ae694c11b9275f5ba6e0d253`
+        url: `https://sandbox.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=1mm&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`
     })
 )
 export const fetchStock3MonthData= (ticker) =>(
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=3m&token=pk_b33e3727ae694c11b9275f5ba6e0d253`
+        url: `https://sandbox.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=3m&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`
     })
 )
 export const fetchStockYearData= (ticker) =>(
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=1y&token=pk_b33e3727ae694c11b9275f5ba6e0d253`
+        url: `https://sandbox.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=1y&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`
     })
 )
 export const fetchStock5YearData= (ticker) =>(
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=5y&token=pk_b33e3727ae694c11b9275f5ba6e0d253`
+        url: `https://sandbox.iexapis.com/v1/stock/${ticker}/batch?&types=chart&range=5y&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`
     })
 )
 
 export const fetchStockStats = (ticker) =>(
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/${ticker}/batch?&types=price,company,stats&token=pk_b33e3727ae694c11b9275f5ba6e0d253`,
+        url: `https://sandbox.iexapis.com/v1/stock/${ticker}/batch?&types=price,company,stats&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`,
     })
 )
 
 export const fetchWeekQuotes = (tickerArr) => (
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/market/batch?&types=chart&range=5dm&symbols=${tickerArr.join(',')}&token=pk_b33e3727ae694c11b9275f5ba6e0d253`,
+        url: `https://sandbox.iexapis.com/v1/stock/market/batch?&types=chart&range=5dm&symbols=${tickerArr.join(',')}&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`,
     })
 )
 
 export const fetchMonthQuotes = (tickerArr) => (
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/market/batch?&types=chart&range=1mm&symbols=${tickerArr.join(',')}&token=pk_b33e3727ae694c11b9275f5ba6e0d253`,
+        url: `https://sandbox.iexapis.com/v1/stock/market/batch?&types=chart&range=1mm&symbols=${tickerArr.join(',')}&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`,
     })
 )
 
 export const fetchThreeMonthsQuotes = (tickerArr) => (
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/market/batch?&types=chart&range=3m&symbols=${tickerArr.join(',')}&token=pk_b33e3727ae694c11b9275f5ba6e0d253`,
+        url: `https://sandbox.iexapis.com/v1/stock/market/batch?&types=chart&range=3m&symbols=${tickerArr.join(',')}&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`,
     })
 )
 
 export const fetchOneYearQuotes = (tickerArr) => (
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/market/batch?&types=chart&range=1y&symbols=${tickerArr.join(',')}&token=pk_b33e3727ae694c11b9275f5ba6e0d253`,
+        url: `https://sandbox.iexapis.com/v1/stock/market/batch?&types=chart&range=1y&symbols=${tickerArr.join(',')}&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`,
     })
 )
 
 export const fetchFiveYearQuotes = (tickerArr) => (
     $.ajax({
         method: "GET",
-        url: `https://cloud.iexapis.com/v1/stock/market/batch?&types=chart&range=5y&symbols=${tickerArr.join(',')}&token=pk_b33e3727ae694c11b9275f5ba6e0d253`,
+        url: `https://sandbox.iexapis.com/v1/stock/market/batch?&types=chart&range=5y&symbols=${tickerArr.join(',')}&token=Tpk_cdcb2f431e914c2cb121acf5f2136e2a`,
     })
 )
